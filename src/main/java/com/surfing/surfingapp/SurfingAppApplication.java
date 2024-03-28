504582e4f635cf6b17d00fa80fb3adc853246358
Paweł <pawel.machnik2@gmail.com>
2024-03-28 18:08:21 +0100

    initial commit

---
+++ src/main/java/com/surfing/surfingapp/SurfingAppApplication.java
@@ -0,0 +1,13 @@
+package com.surfing.surfingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurfingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurfingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/surfing/surfingapp/SurfingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.surfing.surfingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurfingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
